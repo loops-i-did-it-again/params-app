34a69d5361b7f55b957a0e5e5117950fa83d8ecf
dani <dani.zaghian@gmail.com>
2020-09-10 11:43:33 -0700

    adds query guess url

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
     get "/name_path" => "params_examples#name_action"
     get "/guess_query_path" => "params_examples#guess_query_action"
     get "/url_segment_path/:wildcard" => "params_examples#url_segment_action"
+    get "/guess_segment_path/:guess" => "params_examples#guess_query_action"
   end
 end
